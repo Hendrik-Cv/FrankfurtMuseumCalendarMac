4c5a9cd11b81cc5ecb0d253afe28323875dd0139
Hendrik <hendrik@cvetko.de>
2026-06-06 11:33:47 +0200

    Initial Commit

---
+++ FrankfurtMuseumCalendarMac/FrankfurtMuseumCalendarMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrankfurtMuseumCalendarMacApp.swift
+//  FrankfurtMuseumCalendarMac
+//
+//  Created by Hendrik Cvetko on 06.06.26.
+//
+
+import SwiftUI
+
+@main
+struct FrankfurtMuseumCalendarMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
